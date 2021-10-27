ca56e6a7166f367947a25a2dc064bc76886e1ab1
srinivasan.nandakumar <srinivasan.kn@edf.sg>
2021-10-27 10:14:24 +0530

    trymongo.js

--- api/trymongo.js
+++ api/trymongo.js
@@ -3,44 +3,6 @@ const { MongoClient } = require('mongodb');
 
 const url = 'mongodb://localhost/listTracker';
 
-/*
-function testWithCallbacks(callback) {
-    console.log('\n--- testWithCallbacks ---');
-    const client = new MongoClient(url, { useNewUrlParser: true });
-    client.connect(function (err, client) {
-        if (err) {
-            callback(err);
-            return;
-        }
-        console.log('Connected to MongoDB');
-
-        const db = client.db();
-        const collection = db.collection('listTracker');
-
-        const employee = { id: 3, no: '9841113214', name: 'S22nivasan' };
-        collection.insertOne(employee, function (err, result) {
-            if (err) {
-                client.close();
-                callback(err);
-                return;
-            }
-            console.log('Result of insert:\n', result.insertedId);
-            collection.find({ _id: result.insertedId })
-                .toArray(function (err, docs) {
-                    if (err) {
-                        client.close();
-                        callback(err);
-                        return;
-                    }
-                    console.log('Result of find:\n', docs);
-                    client.close();
-                    callback(err);
-                });
-        });
-    });
-}
-*/
-
 async function testWithAsync() {
     console.log('\n--- testWithAsync ---');
     const client = new MongoClient(url, { useNewUrlParser: true });
@@ -66,6 +28,10 @@ async function testWithAsync() {
 
         const final_data = await collection.find().toArray();
         console.log("Final Data", final_data);
+
+
+        const result_del = await collection.deleteMany({});
+
     }
 
     catch (err) {
@@ -78,11 +44,3 @@ async function testWithAsync() {
 
 
 testWithAsync();
-/*
-testWithCallbacks(function (err) {
-    if (err) {
-        console.log(err);
-    }
-
-});
-*/
